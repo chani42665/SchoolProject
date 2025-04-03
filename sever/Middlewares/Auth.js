d530a053219dc26035eb1ff312a29fcd6c441cfa
Chani Fridman <chani42665@gmail.com>
2025-04-03 22:51:15 +0300

    לא ידעת מה השתנה פה ....

--- sever/Middlewares/Auth.js
+++ sever/Middlewares/Auth.js
@@ -2,33 +2,32 @@ const jwt = require("jsonwebtoken")
 
 function createToken(req, res, next) {
     const { email, role } = req.body;
-    const token = jwt.sign({ email,role }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
+    const token = jwt.sign({ email, role }, process.env.JWT_SECRET_KEY, { expiresIn: "1h" })
     req.token = token
     next();
 }
 
-function verify(req, res,next) {
-    try{
+function verify(req, res, next) {
+    try {
         const user = jwt.verify(req.headers["authorization"], process.env.JWT_SECRET_KEY)
-    if(!user)
-        res.status(401).json({ error: "Unauthorized" })
-    req.user = user
-    next()
+        if (!user) {
+            return res.status(401).json({ error: "Unauthorized" })
+        }
+        req.user = user
+        next()
     } catch (error) {
-        res.status(401).json({ error: "Invalid token." })
+        return res.status(401).json({ error: "Invalid token." })
     }
-
-
 }
 
 function authorizeRoles(...roles) {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
-            res.status(403).json({ error: "Access denied. Insufficient permissions." });
+            return res.status(403).json({ error: "Access denied. Insufficient permissions." });
         }
         next();
     };
 }
 
-module.exports ={createToken,verify,authorizeRoles}
+module.exports = { createToken, verify, authorizeRoles }
 
